c8ed76229e6b6c40ef627b80b1a3cffdd25dc6cf
bubblesort <aleksanrmerklin@gmail.com>
2020-12-13 15:17:51 +0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,87 @@
+let ALL_EMOJI;
+const buttonStart = document.querySelector('.start');
+const buttonStop = document.querySelector('.stop');
+const inputField = document.querySelector('.input');
+buttonStart.addEventListener('click', startRender);
+buttonStop.addEventListener('click', stopRender);
+let id = 0;
+let interval;
+let isStart = false;
+
+
+// document.body.style.overflow = 'hidden';
+// document.body.style.backgroundColor = '#fgfghh'
+
+function startRender() {
+  if (inputField.value.trim() === '') {
+    return;
+  }
+  ALL_EMOJI = inputField.value
+    .split(',')
+    .map(item => {
+      return item.trim()
+    })
+    .filter(item => {
+      return item !== '';
+    })
+
+  console.log(ALL_EMOJI)
+
+  //console.log(testEmoji)
+  if (!isStart) {
+    interval = setInterval(() => {
+      renderEmoji();
+    }, 200)
+    isStart = true;
+  }
+
+}
+
+function stopRender() {
+  if (isStart) {
+    clearInterval(interval);
+    isStart = false;
+  }
+}
+
+
+function renderEmoji() {
+  const whichEmoji = getRandom(0, ALL_EMOJI.length - 1);
+  let newEmoji = document.createElement('span');
+  id++
+  newEmoji.setAttribute('id', `${id}`);
+  newEmoji.style.fontSize = `${getRandom(15, 50)}px`;
+  newEmoji.style.position = 'absolute';
+  newEmoji.style.top = '-50px';
+  newEmoji.style.left = `${getRandom(50, window.innerWidth) - 50}px`;
+  let transition = getTransition(parseInt(newEmoji.style.fontSize));
+  newEmoji.style.transition = `all ${transition}s linear`;
+  newEmoji.insertAdjacentHTML('afterbegin', `
+  ${ALL_EMOJI[whichEmoji]}
+  `)
+
+
+  document.body.append(newEmoji);
+  //console.log(transition)
+
+  let currItem;
+
+
+  setTimeout(() => {
+    currItem = document.querySelector(`span[id='${id}']`)
+    currItem.style.top = '100%';
+  }, 100)
+
+
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function getTransition(fz) {
+  return (1 / fz) * 130;
+}
+
+
+
